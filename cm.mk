d4c30be93a352e43de74bd54b99dfd58133eb0ad
Arijit Roy <tukaiari@gmail.com>
2020-05-12 22:04:20 +0530

    Add files

---
+++ cm.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+PRODUCT_NAME := cm_z010
+PRODUCT_DEVICE := z010
+PRODUCT_BRAND := Asus
+PRODUCT_MANUFACTURER := Asus
+PRODUCT_MODEL := Zenfone Max
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo cm_z010-eng
+add_lunch_combo omni_z010-eng
+
